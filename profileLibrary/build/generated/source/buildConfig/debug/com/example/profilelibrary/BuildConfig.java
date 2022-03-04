97451c9dc2ef9b4014bbec7eb8770524d685a2e4
Aman Semlawat <68041570+amansem@users.noreply.github.com>
2022-03-04 11:59:14 +0530

    Add files via upload

---
+++ profileLibrary/build/generated/source/buildConfig/debug/com/example/profilelibrary/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.profilelibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.profilelibrary";
+  public static final String BUILD_TYPE = "debug";
+}
